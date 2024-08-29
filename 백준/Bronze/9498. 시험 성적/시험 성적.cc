d169455af02cbe761d339279ad7e15c38b1ba131
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2024-08-29 20:09:40 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.cc
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -1,28 +1,6 @@
 #include <cstdio>
 
-int main()
-{
-
-    int a;
-    scanf("%d", &a);
-    if (a > 89)
-    {
-        printf("A");
-    }
-    else if (a > 79)
-    {
-        printf("B");
-    }
-    else if (a > 69)
-    {
-        printf("C");
-    }
-    else if (a > 59)
-    {
-        printf("D");
-    }
-    else
-    {
-        printf("F");
-    }
+int main() {
+    int a;scanf("%d",&a);
+    printf("%c","FFFFFFDCBAA"[a/10]);
 }
