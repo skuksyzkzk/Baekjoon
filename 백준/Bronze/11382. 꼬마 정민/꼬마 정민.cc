4f131d362cc2cab8b73000e7a181502ab5979009
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2024-08-29 19:44:09 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    long long num1, num2, num3;
+
+    cin >> num1 >> num2 >> num3;
+    cout << num1 + num2 + num3;
+}
