190b02c071bdcc876825a9bda47d96865c02324b
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2024-08-29 20:32:41 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -1,7 +1,7 @@
-#include <iostream>
-
-int main() {
-    int a,b;scanf("%d %d",&a,&b);
-    printf("%d",a>0?(b>0?1:4):(b>0?2:3));
-    return 0;
+#include <cstdio>
+main() {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d",((b<0)*2
+           )+(a*b<0)+1);
 }
