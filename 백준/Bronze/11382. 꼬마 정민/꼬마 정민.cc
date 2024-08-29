3623027310c48a0a3cb936736f1d2753695b5c04
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2024-08-29 19:51:06 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -1,14 +1,7 @@
-#include <iostream>
-
-using namespace std;
-
-main()
+#include<stdio.h>
+int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    long long num1, num2, num3;
-
-    cin >> num1 >> num2 >> num3;
-    cout << num1 + num2 + num3;
+	long long A,B,C;
+	scanf("%lld%lld%lld",&A,&B,&C);
+	printf("%lld",A+B+C);
 }
