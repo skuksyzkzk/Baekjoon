04f50575daea6d992bbee07704d103d66fec6cde
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2024-08-31 15:26:04 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+// A＠B = (A+B)×(A-B)
+
+int main()
+{
+    long long a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+    cout << ((a * a) + (b * b) + (c * c) + (d * d) + (e * e)) % 10;
+    return 0;
+}
