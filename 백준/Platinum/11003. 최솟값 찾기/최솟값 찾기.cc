6a08579f9e078b92058257f6bac5d0684d8a4c84
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2024-09-29 19:43:04 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 1088 ms, Memory: 43588 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
@@ -3,39 +3,32 @@
 
 using namespace std;
 
-struct Score {
-	int index;
-	int value;
-};
-
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
 	int N, L;
-	deque<Score> dq;
+	deque<pair<int,int>> dq;
 
 	cin >> N >> L;
 
-	for (int i = 1; i <= N; i++) {
-		Score input;
-		input.index = i;
-		cin >> input.value;
 
-		// 덱에서 뒤쪽 원소가 현재 입력 값보다 크면 제거
-		while (!dq.empty() && dq.back().value > input.value) {
+	for (int i = 1; i < N + 1; i++) {
+		pair<int, int> input;
+		input.first = i;
+		cin >> input.second;
+
+
+		while (!dq.empty() && dq.back().second > input.second) {
 			dq.pop_back();
 		}
-
-		// 현재 값을 덱에 삽입
 		dq.push_back(input);
 
-		// 덱의 앞쪽 원소가 윈도우 범위를 벗어나면 제거
-		if (dq.front().index <= i - L) {
+		if (dq.front().first <= input.first - L) {
 			dq.pop_front();
 		}
 
-		// 덱의 맨 앞에 있는 값이 현재 슬라이딩 윈도우에서의 최솟값
-		cout << dq.front().value << " ";
+		cout << dq.front().second << " ";
+
+
 	}
 
 	return 0;
