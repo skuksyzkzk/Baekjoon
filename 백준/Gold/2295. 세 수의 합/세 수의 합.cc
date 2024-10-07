4fb807db24d67171082dbfc7707a4e69ffea2cb4
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2024-10-07 15:25:58 +0900

    [Gold IV] Title: 세 수의 합, Time: 28 ms, Memory: 5228 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.cc
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.cc
@@ -3,39 +3,33 @@
 #include <algorithm>
 
 using namespace std;
-
+int arr[1002];
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	int N;
 	cin >> N;
-	vector<int> v(N,0);
 	for (int i = 0; i < N; i++) {
-		cin >> v[i];
+		cin >> arr[i];
 	}
+	sort(arr, arr + N);
 
-	sort(v.begin(), v.end());
-	
-	// 2개의 수의 합 배열 
 	vector<int> two;
 	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			two.push_back(v[i] + v[j]);
+		for (int j = i; j < N; j++) {
+			two.push_back(arr[i] + arr[j]);
 		}
 	}
 	two.erase(unique(two.begin(), two.end()), two.end());
 	sort(two.begin(), two.end());
-	int max{ 0 };
-	for (int i = N-1; i > 0; i--) {
+
+	for (int i = N - 1; i >= 0; i--) {
 		for (int j = 0; j < i; j++) {
-			if (binary_search(two.begin(), two.end(), v[i] - v[j])) {
-				cout << v[i];
+			if (binary_search(two.begin(), two.end(), arr[i] - arr[j])) {
+				cout << arr[i];
 				return 0;
 			}
 		}
 	}
-
-	
-
 
 	return 0;
 }
