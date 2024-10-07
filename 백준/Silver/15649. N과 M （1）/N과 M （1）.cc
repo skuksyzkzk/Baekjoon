70808b9da9a244bfbbdf58aa91ccfa5fa3647263
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2024-10-08 00:55:38 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,24 +1,22 @@
 #include <iostream>
 
 using namespace std;
-
 int N, M;
 int print[10];
-bool vis[10];
-
-void permutation(int k) {
+int vis[10];
+void p(int k) {
 	if (k == M) {
 		for (int i = 0; i < M; i++)
-			cout << print[i] << " ";
-		cout << "\n";
+			cout << print[i] << ' ';
+
+		cout << '\n';
 	}
-		
-	
+
 	for (int i = 1; i <= N; i++) {
 		if (!vis[i]) {
-			vis[i] = 1;
 			print[k] = i;
-			permutation(k + 1);
+			vis[i] = 1;
+			p(k + 1);
 			vis[i] = 0;
 		}
 	}
@@ -26,7 +24,6 @@ void permutation(int k) {
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> N >> M;
-	permutation(0);
-
+	p(0);
 	return 0;
 }
