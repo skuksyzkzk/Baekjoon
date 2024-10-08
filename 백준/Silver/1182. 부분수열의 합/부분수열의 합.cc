cb54f962724b1b0bf5581bffb98be0386056c064
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2024-10-08 17:28:34 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -1,34 +1,30 @@
 #include <iostream>
 
 using namespace std;
-
 int N, S;
-int cnt = 0;
 int arr[22];
+int cnt = 0;
+void c(int level, int total) {
+	if (level == N) {
+		if (total == S) cnt++;
+		return;
+	}
 
-void c(int level, int sum, int start) {
-    // 부분집합이 하나 이상 선택된 경우에만 체크
-    if (level > 0 && sum == S) {
-        cnt++;
-    }
-    // start부터 N까지 부분집합을 구성
-    for (int i = start; i < N; i++) {
-        c(level + 1, sum + arr[i], i + 1);
-    }
+	c(level + 1, total);
+	c(level + 1, total + arr[level]);
+
+	
+
 }
-
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    cin >> N >> S;
-    
-    for (int i = 0; i < N; i++) {
-        cin >> arr[i];
-    }
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> N >> S;
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+	c(0, 0);
 
-    // 재귀를 통해 모든 부분집합을 탐색
-    c(0, 0, 0);
-    
-    // 출력
-    cout << cnt;
-    return 0;
+	if (S == 0) cnt--;
+	cout << cnt;
+	return 0;
 }
