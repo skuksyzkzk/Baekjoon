05f87c4e3e122a02f2178ac68b0d137cd9b14e74
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2024-10-31 20:13:05 +0900

    [level 2] Title: 올바른 괄호, Time: 14.07 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,17 +2,15 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        
-        Stack<Character> stack = new Stack<>();
-        for (int i =0;i<s.length();i++){
-            if(s.charAt(i) == '(') stack.push('(');
-            else{
-                if (stack.isEmpty()) return false;
-                if (!stack.isEmpty() && stack.peek() != '(') return false;
-                if (!stack.isEmpty()) stack.pop();
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        char[] cs = s.toCharArray();
+        for (char c : cs){
+            if (c == '(') stack.push(c);
+            else {
+                if (stack.isEmpty() || stack.pop() == c) return false;
             }
         }
-        
-        return stack.isEmpty();
+
+        return stack.isEmpty() ;
     }
 }
