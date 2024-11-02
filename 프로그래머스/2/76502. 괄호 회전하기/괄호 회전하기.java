661bad6cbcb88c0673fe05c9de55414acc0f3c43
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2024-11-02 15:40:52 +0900

    [level 2] Title: 괄호 회전하기, Time: 35.60 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -3,44 +3,33 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        // 문자열 저장 Deque
-        ArrayDeque<Character> list = new ArrayDeque<>();
-        for (char c : s.toCharArray()){
-            list.addLast(c);
-        }
-        
-        // 짝 맞춰놓기 
-        Map<Character,Character> map = new HashMap<>();
+        Map<Character, Character> map = new HashMap<>();
         map.put(')','(');
         map.put(']','[');
         map.put('}','{');
         
         int strSize = s.length();
-        for (int i = 0;i < strSize;i++){
+        for (int startIdx = 0; startIdx < strSize;startIdx++){
             ArrayDeque<Character> stack = new ArrayDeque<>();
-            boolean isVaild = true;
-            for (char target : list){
+            boolean flag  = true;
+            for (int i = startIdx; i < startIdx + strSize; i++ ){
+                int accessIdx = i % strSize;
+                char target = s.charAt(accessIdx);
                 if (!map.containsKey(target)) stack.push(target);
                 else {
                     if (stack.isEmpty()){
-                        isVaild = false;
+                        flag = false;
                         break;
                     }
                     else if (!map.get(target).equals(stack.pop())){
-                        isVaild =false ;
+                        flag = false;
                         break;
                     }
-                
                 }
                 
             }
-            if (isVaild && stack.isEmpty()) answer++;
-                
-            char temp = list.poll();
-            list.addLast(temp);
+            if (flag && stack.isEmpty()) answer++;
         }
-        
-        
         return answer;
     }
 }
