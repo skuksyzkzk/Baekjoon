7929d01e382663f6dc4bdb6a6855bc3a76942230
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2024-11-03 18:04:57 +0900

    [level 2] Title: 기능개발, Time: 3.17 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,31 +1,18 @@
-import java.util.*;
+
+// 문제가 개편되었습니다. 이로 인해 함수 구성이나 테스트케이스가 변경되어, 과거의 코드는 동작하지 않을 수 있습니다.
+// 새로운 함수 구성을 적용하려면 [코드 초기화] 버튼을 누르세요. 단, [코드 초기화] 버튼을 누르면 작성 중인 코드는 사라집니다.
+import java.util.ArrayList;
+import java.util.Arrays;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> answer = new ArrayList<>();
-        int[] dayLeft = new int[progresses.length];
-        // 날짜를 계산 하기 
-        for (int idx = 0; idx < progresses.length; idx ++) {
-            if ((100 - progresses[idx]) % speeds[idx] == 0)  
-                dayLeft[idx] = (100 - progresses[idx]) / speeds[idx];
-            else 
-                dayLeft[idx] = (100 - progresses[idx]) / speeds[idx] + 1;
-        }
-        
-        int maxDay = dayLeft[0];
-        int count = 0;
-        
-        for (int i = 0 ; i < dayLeft.length;i++){
-            if (dayLeft[i] <= maxDay) {
-                count++;
+        int[] dayOfend = new int[100];
+        int day = -1;
+        for(int i=0; i<progresses.length; i++) {
+            while(progresses[i] + (day*speeds[i]) < 100) {
+                day++;
             }
-            else {
-                maxDay  = dayLeft[i];
-                answer.add(count);
-                count = 1;
-            }
+            dayOfend[day]++;
         }
-        answer.add(count);
-        
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        return Arrays.stream(dayOfend).filter(i -> i!=0).toArray();
     }
 }
