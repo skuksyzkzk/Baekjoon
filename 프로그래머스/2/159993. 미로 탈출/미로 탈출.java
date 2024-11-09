ec08618498ac9095ace8cd59bb442680120c5b9c
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2024-11-10 01:27:58 +0900

    [level 2] Title: 미로 탈출, Time: 8.22 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,99 +1,74 @@
-import java.util.ArrayDeque;
+import java.util.*;
+class Pair {
+    int x, y;
+    public Pair(int x,int y){
+        this.x = x ;
+        this.y = y ; 
+    }
+}
+class Solution {
+    private static int n,m;
+    private static int[][] dist;
+    private static final int[] dx = {1,-1,0,0};
+    private static final int[] dy = {0,0,1,-1};
 
-public class Solution {
-
-    // ❶ 위, 아래, 왼쪽, 오른쪽 이동 방향
-    private static final int[] dx = {0, 0, -1, 1};
-    private static final int[] dy = {-1, 1, 0, 0};
-
-    // ❷ 위치 정보(x, y)를 저장할 클래스 생성
-    private static class Point {
-        int nx, ny;
-
-        public Point(int nx, int ny) {
-            this.nx = nx;
-            this.ny = ny;
+    private static Pair start,end;
+    
+    private static void initDist(){
+        for(int[] i : dist){
+            Arrays.fill(i,-1);
         }
     }
-
-    private static char[][] map;
-    private static int N, M;
-
+    
     public int solution(String[] maps) {
-        N = maps.length;
-        M = maps[0].length();
-
-        // ❸ 미로에 대한 정보를 배열로 저장
-        map = new char[N][M];
-        for (int i = 0; i < N; i++) {
+        n = maps.length;
+        m = maps[0].length();
+        // char[][] 
+        char[][] map = new char[n][m];
+        for (int i = 0; i < n; i++){
             map[i] = maps[i].toCharArray();
         }
-
-        Point start = null, end = null, lever = null;
-
-        // ❹ 시작 지점, 출구 그리고 레버의 위치를 찾음
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] == 'S') start = new Point(j, i);
-                else if (map[i][j] == 'E') end = new Point(j, i);
-                else if (map[i][j] == 'L') lever = new Point(j, i);
+        // start,end 저장 
+        for (int i = 0; i<n;i++){
+            for (int j = 0; j < m; j++){
+                if (map[i][j] == 'S')
+                    start = new Pair(i,j);
+                if (map[i][j] == 'E')
+                    end = new Pair(i,j);
             }
         }
-
-        // ❺ 시작 자점 -> 레버, 레버 -> 출구까지의 최단 거리를 각각 구함
-        int startLever = bfs(start, lever);
-        int leverEnd = bfs(lever, end);
-
-        // ❻ 도착 불가능한 경우는 -1, 도착 가능한 경우는 최단 거리를 반환
-        if (startLever == -1 || leverEnd == -1)
-            return -1;
-        else
-            return startLever + leverEnd;
-    }
-
-    // start -> end 로 너비 우선 탐색하여 최단거리 반환
-    private static int bfs(Point start, Point end) {
-        // ❼ 너비 우선 탐색 초기 과정
-        int[][] dist = new int[N][M];
-        ArrayDeque<Point> queue = new ArrayDeque<>();
-
-        dist[start.ny][start.nx] = 1;
-        queue.add(start);
-
-        while (!queue.isEmpty()) {
-            Point now = queue.poll();
-
-            // ❽ 네 방향으로 이동
-            for (int i = 0; i < 4; i++) {
-                int nextX = now.nx + dx[i];
-                int nextY = now.ny + dy[i];
-
-                // ❾ 범위를 벗어나는 경우 예외 처리
-                if (nextX < 0 || nextX >= M || nextY < 0 || nextY >= N)
-                    continue;
-
-                // ➓ 이미 방문한 지점인 경우 탐색하지 않음
-                if (dist[nextY][nextX] > 0)
-                    continue;
-
-                // ⓫ X가 아닌 지점만 이동 가능
-                if (map[nextY][nextX] == 'X')
-                    continue;
-
-                // ⓬ 거리 1증가
-                dist[nextY][nextX] = dist[now.ny][now.nx] + 1;
-
-                // ⓭ 다음 정점을 큐에 넣음
-                queue.add(new Point(nextX, nextY));
-
-                // ⓮ 도착점에 도달하면 최단 거리를 반환
-                if (nextX == end.nx && nextY == end.ny)
-                    return dist[end.ny][end.nx] - 1;
+        // dist, queue 초기화 
+        dist = new int[n][m];
+        initDist();
+        dist[start.x][start.y] = 0;
+        ArrayDeque<Pair> q = new ArrayDeque<>();
+        q.offer(new Pair(start.x,start.y));
+        boolean flag = false;
+        // Lever 만날때까지 일단 BFS 진행 
+        while(!q.isEmpty()){
+            Pair cur = q.poll();
+            for (int dir = 0; dir < 4; dir++){
+                int nx = cur.x + dx[dir];
+                int ny = cur.y + dy[dir];
+                if (nx < 0 || nx >= n|| ny < 0 || ny >=m) continue;
+                if (map[nx][ny] == 'X'||dist[nx][ny] > 0) continue;
+                
+                dist[nx][ny] = dist[cur.x][cur.y] + 1 ;
+                if (map[nx][ny] == 'L'){
+                    q.clear();
+                    int lever = dist[nx][ny];
+                    initDist();
+                    dist[nx][ny] = lever;
+                    q.offer(new Pair(nx,ny));
+                    flag = true;
+                    break;
+                }
+                q.offer(new Pair(nx,ny));
             }
         }
-
-        // ⓯ 탐색을 종료할 때까지 도착 지점에 도달하지 못 했다면 -1 반환
-        return -1;
+        if (flag) return dist[end.x][end.y] == -1 ? -1 : dist[end.x][end.y];
+        else return -1;
+        
+        
     }
-
 }
