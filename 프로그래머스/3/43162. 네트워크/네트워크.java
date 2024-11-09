87b8472651092a1e0b8c40b3d8ed085d76aea22a
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2024-11-09 18:48:26 +0900

    [level 3] Title: 네트워크, Time: 0.41 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,52 +2,28 @@ import java.util.*;
 
 class Solution {
     private static boolean[] visited;
-    private static ArrayList<Integer>[] adj;
+    private static int[][] computer;
+    private static void bfs(int start){
+        visited[start] = true;
+        
+        for (int i = 0; i < computer[start].length;i++){
+            if (computer[start][i] == 1 && !visited[i]){
+                bfs(i);
+            }
+        }
+    }
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        adj = new ArrayList[n];
-        for (int i = 0; i< n ; i++){
-            adj[i] = new ArrayList<>();
-        }
-        for (int i = 0 ;i <n-1;i++){
-            for (int j = i+1; j<n; j++){
-                if (computers[i][j] == 1){
-                    adj[i].add(j);
-                    adj[j].add(i); 
-                }
-                
-            }
-        }
         visited = new boolean[n];
+        computer =computers;
         
-        ArrayDeque<Integer> q = new ArrayDeque<>();
-        for (int i = 0 ; i <n ; i++){
-            q.offer(i);
-        }
-      
-        while(!q.isEmpty()){
-            int cur = q.poll();
-            if (visited[cur]) continue;
-            else {
+        for (int i = 0; i<n;i++){
+            if(!visited[i]){
                 answer++;
-                bfs(cur);
+                bfs(i);
             }
         }
-        return answer;
-    }
-    private static void bfs(int start){
-        ArrayDeque<Integer> stack = new ArrayDeque<>();
-        stack.push(start);
         
-        while(!stack.isEmpty()){
-            int cur = stack.pop();
-            visited[cur] = true;
-            System.out.println(cur);
-            for (int next : adj[cur]){
-                if(!visited[next]){
-                    stack.push(next);
-                }
-            }
-        }
+        return answer;
     }
 }
