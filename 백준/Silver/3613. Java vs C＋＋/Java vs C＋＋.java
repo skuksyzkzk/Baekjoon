8ce1fd6321e7819bba6f9fbd9d43bc70f7554166
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2024-11-17 00:24:40 +0900

    [Silver III] Title: Java vs C++, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/3613. Java vs C＋＋/Java vs C＋＋.java
+++ 백준/Silver/3613. Java vs C＋＋/Java vs C＋＋.java
@@ -1,91 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
+
 
 public class Main {
-
-    static boolean whatIsLanguage = false;     // false: c++, true: java
-    static boolean allOfLower = true;
-
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder(br.readLine());
 
-        // Java, C++ 형식에 맞지 않을 경우
-        if(!(is_Cpp(sb) && is_Java(sb) && name_check(sb))){
-            System.out.println("Error!");
-            return;
+        boolean java = false ;
+        boolean c = false;
+        
+        for (int i =0; i<sb.length();i++) {
+        	if (sb.charAt(i) == '_') {
+        		c = true;
+        	}
+        	if (sb.charAt(i) >= 'A' && sb.charAt(i) <= 'Z') {
+        		java = true;
+        	}
         }
-
-        // 모든 문자가 소문자일 경우 그대로 return
-        if(allOfLower){
-            System.out.println(sb.toString());
-            return;
+        // java : 맨 처음에 대문자 일경우 문제
+        // c : _가 처음이나 , _이 끝에 있을 경우, __이 두개 연속 반복될경우
+        if (sb.charAt(0) >= 'A' && sb.charAt(0) <= 'Z') {
+        	System.out.println("Error!");
+        	return;
         }
-
-        // true = java, false = Cpp
-        if(whatIsLanguage) sb = javaToCpp(sb);
-        else sb = cppToJava(sb);
-
-        System.out.println(sb.toString());
-
-    }
-
-    public static boolean name_check(StringBuilder sb){
-        int isUpperCase = 0;
-        int isUnderBar = 0;
-
-       for(int i=0; i < sb.length(); i++){
-           char ch = sb.charAt(i);
-
-           if(ch >= 'A' && ch <= 'Z') isUpperCase = 1;
-           else if(ch == '_') isUnderBar = 1;
-       }
-
-       allOfLower = (isUnderBar == 1 || isUpperCase == 1) ? false : true;
-
-        // Java와 Cpp 형식을 혼용 했을때 false return
-       if((isUnderBar & isUpperCase) == 0 || allOfLower) {
-           whatIsLanguage = (isUnderBar == 1) ? false : true;
-           return true;
-       }else return false;
-
-    }
-
-    public static boolean is_Cpp(StringBuilder sb){
-        if(sb.charAt(sb.length()-1) == '_') return false;
-        else if(sb.charAt(0) == '_') return false;
-        else if(sb.indexOf("__") >= 0) return false;
-
-        return true;
-    }
-
-    public static boolean is_Java(StringBuilder sb){
-        return (sb.charAt(0) >= 'A'&& sb.charAt(0) <= 'Z') ? false : true;
-    }
-
-    public static StringBuilder javaToCpp(StringBuilder sb){
-        for(int i=0; i<sb.length(); i++){
-            char ch = sb.charAt(i);
-            if(ch >= 'A' && ch <= 'Z'){
-                sb.replace(i, i+1, String.valueOf(ch).toLowerCase());
-                sb.insert(i, "_");
-            }
+        if (sb.charAt(0) == '_' || sb.charAt(sb.length()-1) == '_'
+        		|| sb.indexOf("__") >0 || (java && c)) {
+        	System.out.println("Error!");
+        	return;
         }
-        return sb;
-    }
-
-    public static StringBuilder cppToJava(StringBuilder sb){
-        for(int i=0; i<sb.length(); i++){
-            if(sb.charAt(i) == '_'){
-                sb.deleteCharAt(i);
-                sb.replace(i+1, i+1, String.valueOf(sb.charAt(i)).toUpperCase());
-                sb.deleteCharAt(i);
-            }
+        
+        if (java) {
+        	for (int i= 0; i< sb.length();i++) {
+        		if (sb.charAt(i) >= 'A' && sb.charAt(i) <= 'Z') {
+        			sb.replace(i,i+1,String.valueOf(sb.charAt(i)).toLowerCase());
+        			sb.insert(i, "_");
+        		}
+        	}
+        } 
+        if (c){
+        	for (int i= 0; i< sb.length();i++) {
+        		if (sb.charAt(i) == '_') {
+        			sb.deleteCharAt(i);
+        			sb.replace(i, i+1, String.valueOf(sb.charAt(i)).toUpperCase());
+        		}
+        	}
         }
-        return sb;
+        System.out.println(sb);
+  
+        
+        
     }
-
-
-
 }
