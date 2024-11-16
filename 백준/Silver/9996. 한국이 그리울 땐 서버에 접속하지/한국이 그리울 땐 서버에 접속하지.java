320d83c1583950a41b9c2765bb078b98c64ea7f8
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2024-11-16 22:54:49 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
@@ -2,25 +2,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb =new StringBuilder();
-		
-		int N = Integer.parseInt(br.readLine());
-		String pattern = br.readLine();
-		String[] given = pattern.split("\\*");
-		String first = given[0];
-		String second = given[1];
-	
-		for (int i = 0 ; i< N ; i++) {
-			String pb = br.readLine();
-			if ( pb.length() >= first.length() + second.length() &&
-					pb.startsWith(first) && pb.endsWith(second)) {
-				sb.append("DA\n");
-			}else sb.append("NE\n");
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        // 입력: 첫 줄에는 N과 패턴
+        int N = Integer.parseInt(br.readLine());
+        String[] inputs = br.readLine().split("\\*");
+        String first = inputs[0];
+        String second = inputs[1];
+        
+        for (int i = 0; i < N; i++) {
+            String testString = br.readLine();
+            if (testString.length() < first.length() + second.length()) {
+            	sb.append("NE\n");
+            	continue;
+            }
+            if (testString.startsWith(first) && testString.endsWith(second)) {
+            	sb.append("DA\n");
+            }
+            else {
+            	sb.append("NE\n");
+            }
+            
+        }
+
+        System.out.print(sb); // 결과 출력
+    }
 }
