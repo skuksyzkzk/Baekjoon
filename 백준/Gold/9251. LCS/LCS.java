462df3628cfc3ee8ebc790b94b0492dddcb5e507
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2024-11-18 16:48:25 +0900

    [Gold V] Title: LCS, Time: 140 ms, Memory: 18448 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		char[] first = br.readLine().toCharArray();
+		char[] second = br.readLine().toCharArray();
+		int[][] lcs = new int [first.length+1][second.length+1];
+		lcs[0][0] = 0;
+		for (int i = 1; i <= first.length; i++) {
+			lcs[i][0] = 0;
+		}
+		for (int i = 1; i <= second.length; i++) {
+			lcs[0][i] = 0;
+		}
+		// i,j 둘이 비교하면서 
+		// Lcs(i,j) 위치에서 first[i]와 second[j]가 같다면 lcs(i-1,j-1) + 1
+		// Lcs(i,j) 위치에서 다르다면 lcs(i-1,j) Lcs(i,j-1) 중 최댓값 
+		for (int i = 1; i<= first.length;i++) {
+			for (int j = 1; j <= second.length; j++) {
+				if (first[i-1] == second[j-1]) lcs[i][j] = lcs[i-1][j-1] + 1;
+				else lcs[i][j] = Math.max(lcs[i-1][j], lcs[i][j-1]);
+			}
+		}
+		// 이제 남은것은 최대 값만 구하면 끝 
+		int maxValue = 0;
+		for (int [] i : lcs) {
+			for (int j : i) {
+				if (maxValue < j) maxValue = j;
+			}
+		}
+		System.out.println(maxValue);
+	}
+
+}
