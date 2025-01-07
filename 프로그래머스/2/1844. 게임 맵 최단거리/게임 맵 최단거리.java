8f0736dc8c45d6d04a952a33dbfa21d58948eaa9
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-01-07 19:57:12 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.84 ms, Memory: 55.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,45 +1,46 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
+    class Pair {
+        int X,Y;
+        public Pair(int X,int Y){
+            this.X = X;
+            this.Y = Y;
+        }
+    }
     static int[] dx = {1,-1,0,0};
     static int[] dy = {0,0,1,-1};
-    static int n ;
-    static int m;
-    static class Pair {
-        int x,y;
-        public Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-    static boolean isOut(int x,int y){
-        return (x < 0 || x >= n || y < 0 || y >= m);
-    }
+    
     public int solution(int[][] maps) {
         int answer = 0;
-        n = maps.length;
-        m = maps[0].length;
-        int[][] dist = new int[n+1][m+1];
-        ArrayDeque<Pair> q = new ArrayDeque<>();
-        // 초기값 설정 
-        for (int[] i : dist) {
-            Arrays.fill(i,-1);
+        // 0은 벽 1은 벽이 아님 
+        int n  = maps.length;
+        int m  = maps[0].length;
+        
+        int [][] dist = new int[n][m];
+        // BFS로 최단거리 해결하는 문제 dist로 판단 처음에 dist를 -1로 채우기 
+        for (int i = 0; i < n;i++){
+            Arrays.fill(dist[i], -1);
         }
-        q.offer(new Pair(0,0));
+        ArrayDeque<Pair> q = new ArrayDeque<>();
+        q.offerLast(new Pair(0,0));
         dist[0][0] = 1;
         
-        while(!q.isEmpty()){
+        // 해당 맵을 나가면, 벽이면, 이미 방문 했으면 x 아닐 경우는 갱신하기
+        // 막혀있으면 -1 아니면 해당거리 출력
+        while (!q.isEmpty()){
             Pair cur = q.poll();
-            for (int dir = 0; dir < 4; dir ++){
-                int nx = cur.x + dx[dir];
-                int ny = cur.y + dy[dir];
-                if(isOut(nx,ny)) continue;
-                if(maps[nx][ny] != 1 || dist[nx][ny] > 1) continue;
-                dist[nx][ny] = dist[cur.x][cur.y] + 1 ;
-                q.offer(new Pair(nx,ny));
+            for (int dir = 0; dir<4;dir++){
+                int newX = cur.X + dx[dir];
+                int newY = cur.Y + dy[dir];
+                if (newX <0 || newX >= n || newY <0 || newY >= m ) continue;
+                if (dist[newX][newY] > 0 || maps[newX][newY] == 0) continue;
+                
+                q.offerLast(new Pair(newX,newY));
+                dist[newX][newY] = dist[cur.X][cur.Y] + 1;
             }
         }
-        if (dist[n-1][m-1] == -1) return -1;
-        else return dist[n-1][m-1];
+        return dist[n-1][m-1] == -1 ? -1 : dist[n-1][m-1];
     }
 }
