a692c13cdad1965b47209e58f99f4921f37e9ffc
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-01-07 20:16:00 +0900

    [level 3] Title: 네트워크, Time: 0.56 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,29 +1,39 @@
 import java.util.*;
 
 class Solution {
-    private static boolean[] visited;
-    private static int[][] computer;
-    private static void bfs(int start){
-        visited[start] = true;
-        
-        for (int i = 0; i < computer[start].length;i++){
-            if (computer[start][i] == 1 && !visited[i]){
-                bfs(i);
-            }
-        }
-    }
+    static int answer = 0;
+    static ArrayList<Integer>[] adj;
+    static boolean[] visited;
     public int solution(int n, int[][] computers) {
-        int answer = 0;
+        // adj 리스트로 무방향 그래프 형성하기 
+        adj = new ArrayList[n];
         visited = new boolean[n];
-        computer =computers;
-        
+        for (int i =0 ; i< n;i++){
+            adj[i] = new ArrayList<>();
+        }
+        // 노드갯수만큼 반복해서 방문하지않는 경우 dfs시작한뒤 dfs실행한 갯수를 체크하기 
         for (int i = 0; i<n;i++){
-            if(!visited[i]){
-                answer++;
-                bfs(i);
+            for (int j = i+1; j < n; j++){
+                if (computers[i][j] == 1){
+                    adj[i].add(j);
+                    adj[j].add(i);
+                }
             }
         }
-        
+        for (int i= 0; i< n;i++){
+            if (!visited[i]){
+                answer++;
+                dfs(i);  
+            } 
+        }
         return answer;
+    }
+    public void dfs(int now){
+        visited[now] = true;
+        for (int next : adj[now]){
+            if(!visited[next]){
+                dfs(next);
+            }
+        }
     }
 }
