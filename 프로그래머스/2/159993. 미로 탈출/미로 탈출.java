677878146c5c819be66df1bf0d6f2806ca3db750
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-01-07 21:26:20 +0900

    [level 2] Title: 미로 탈출, Time: 6.34 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,74 +1,75 @@
 import java.util.*;
-class Pair {
-    int x, y;
-    public Pair(int x,int y){
-        this.x = x ;
-        this.y = y ; 
-    }
-}
-class Solution {
-    private static int n,m;
-    private static int[][] dist;
-    private static final int[] dx = {1,-1,0,0};
-    private static final int[] dy = {0,0,1,-1};
 
-    private static Pair start,end;
-    
-    private static void initDist(){
+class Solution {
+    class Pair {
+        int X,Y;
+        public Pair (int X,int Y){
+            this.X = X;
+            this.Y = Y;
+        }
+    }
+    static int[] dx = {1,-1,0,0};
+    static int[] dy = {0,0,1,-1};
+    public int solution(String[] maps) {
+        int answer = 0;
+        boolean lever = false;
+        int n = maps.length;
+        int m = maps[0].length();
+        Pair start = null;
+        Pair exit = null;
+        int[][] dist = new int[n][m];
         for(int[] i : dist){
             Arrays.fill(i,-1);
         }
-    }
-    
-    public int solution(String[] maps) {
-        n = maps.length;
-        m = maps[0].length();
-        // char[][] 
+        // 변환 시키자 int값들로 이루어진 배열로 일단 
         char[][] map = new char[n][m];
-        for (int i = 0; i < n; i++){
+        for (int i= 0; i<n;i++){
             map[i] = maps[i].toCharArray();
         }
-        // start,end 저장 
-        for (int i = 0; i<n;i++){
-            for (int j = 0; j < m; j++){
-                if (map[i][j] == 'S')
+        for (int i = 0 ; i < n;i++){
+            for (int j = 0 ; j< m; j++){
+                if (map[i][j] == 'S'){
                     start = new Pair(i,j);
-                if (map[i][j] == 'E')
-                    end = new Pair(i,j);
+                }
+                else if (map[i][j] == 'E'){
+                    exit = new Pair(i,j);
+                }
             }
         }
-        // dist, queue 초기화 
-        dist = new int[n][m];
-        initDist();
-        dist[start.x][start.y] = 0;
         ArrayDeque<Pair> q = new ArrayDeque<>();
-        q.offer(new Pair(start.x,start.y));
-        boolean flag = false;
-        // Lever 만날때까지 일단 BFS 진행 
-        while(!q.isEmpty()){
+        q.offer(new Pair(start.X,start.Y));
+        dist[start.X][start.Y] = 0;
+        // 일단 동서남북으로 움직임 일단 첫번째 목표는 레버까지 가는 것 
+        // 레버를 가면 거기서 레버까지 dist 제외 초기화 
+        // 다시 시작해서 EXIT까지 측정 
+        // 레버를 댕겨야 됨 , 출구도 찍혀있어야된다. 
+        while (!q.isEmpty()){
             Pair cur = q.poll();
+            
             for (int dir = 0; dir < 4; dir++){
-                int nx = cur.x + dx[dir];
-                int ny = cur.y + dy[dir];
-                if (nx < 0 || nx >= n|| ny < 0 || ny >=m) continue;
-                if (map[nx][ny] == 'X'||dist[nx][ny] > 0) continue;
-                
-                dist[nx][ny] = dist[cur.x][cur.y] + 1 ;
-                if (map[nx][ny] == 'L'){
+                int newX = cur.X + dx[dir];
+                int newY = cur.Y + dy[dir];
+                if (newX < 0 || newX >= n || newY < 0 || newY >=m) continue;
+                if (map[newX][newY] == 'X' || dist[newX][newY] > 0) continue;
+                dist[newX][newY] = dist[cur.X][cur.Y] + 1;
+                if (map[newX][newY] == 'L' && !lever){
+                    lever = true;
+                    int temp = dist[newX][newY];
+                    for (int[] es : dist){
+                        Arrays.fill(es,-1);
+                    }
                     q.clear();
-                    int lever = dist[nx][ny];
-                    initDist();
-                    dist[nx][ny] = lever;
-                    q.offer(new Pair(nx,ny));
-                    flag = true;
+                    q.push(new Pair(newX,newY));
+                    dist[newX][newY] = temp;
                     break;
                 }
-                q.offer(new Pair(nx,ny));
+                q.offer(new Pair(newX,newY));
             }
         }
-        if (flag) return dist[end.x][end.y] == -1 ? -1 : dist[end.x][end.y];
-        else return -1;
-        
-        
+        if (lever && dist[exit.X][exit.Y] != -1){
+            return dist[exit.X][exit.Y];
+        }else {
+            return -1;
+        }
     }
 }
