e066482d44d0ee9749e464b60dc86b0386dc7a3a
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-01-08 14:38:58 +0900

    [level 2] Title: 배달, Time: 5.87 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,51 +1,60 @@
 import java.util.*;
-// 노드 클래스 - 인터페이스로 비교(최단거리 기준 )
-class Node implements Comparable<Node> {
-    int dest,cost;
-    public Node(int dest,int cost){
-        this.dest = dest;
-        this.cost = cost;
-    }
-    @Override
-    public int compareTo(Node other){
-        return Integer.compare(this.cost,other.cost);
-    }
-}
+
 class Solution {
+    class Node  implements Comparable<Node> {
+        int dest,cost;
+        public Node (int dest,int cost){
+            this.dest = dest;
+            this.cost = cost;
+        }
+        
+        @Override 
+        public int compareTo(Node other){
+            return Integer.compare(this.cost,other.cost);
+        }
+        
+    }
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
-        // 우선순위 큐 -> 주변 노드를 푸시하지만 최단거리 순 
-        ArrayList<Node>[] adj = new ArrayList[N+1];
-        for (int i = 0;i<=N;i++){
-            adj[i] = new ArrayList<>();
-        }
-        for(int[] i : road){
-            adj[i[0]].add(new Node(i[1],i[2]));
-            adj[i[1]].add(new Node(i[0],i[2]));
-        }
-        // dist의 배열에 거리 보다 큐 poll한게 크다 그러면 이미 결정된 최단거리 이므로 넘김
+        // 1번에서 다익스트라로 가는 경로 비용이 K 만큼보다 작아야되는거니까
+        // 다익스트라 한번 해서 비용 갱신하고 거기서 K보다 작은거 확인
         int[] dist = new int[N+1];
         Arrays.fill(dist,Integer.MAX_VALUE);
+        ArrayList<Node>[] adj = new ArrayList[N+1];
+        for (int i = 0 ; i<= N; i++){
+            adj[i] = new ArrayList<>();
+        }
+        for (int i = 0 ; i<road.length;i++){
+            adj[road[i][0]].add(new Node(road[i][1],road[i][2]));
+            adj[road[i][1]].add(new Node(road[i][0],road[i][2]));
+        }
+        // 다익스트라 과정 
+        // 한 지점에서 최솟값으로 우선순위큐로 해서 제일 최소 비용인 간선기준 
+        // 현재 코스트가 해당 지점의 cost보다 작으면 무시 
+        // 현재 코스트가 cur cost + 라인의 cost 보다 작으면 이 역시 무시 
+        // 아닐 경우 코스트를 위의 값으로 갱신 
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(1,0));
         dist[1] = 0;
-        PriorityQueue<Node> pq  = new PriorityQueue<>();
-        pq.offer(new Node(1,0));
-        //거리를 초기화 = 기존 거리 > 해당 노드까지 + 노드 코스트, 큐에 푸시 (왜냐 간선이니까)
+        
         while(!pq.isEmpty()){
             Node cur = pq.poll();
-            
             if (dist[cur.dest] < cur.cost) continue;
             
-            for (Node node : adj[cur.dest]){
-                if (dist[node.dest] > dist[cur.dest] + node.cost){
-                    dist[node.dest] = dist[cur.dest] + node.cost;
-                    pq.offer(new Node(node.dest,dist[node.dest]));
+            for (Node next : adj[cur.dest]){
+                if (dist[next.dest] > cur.cost + next.cost) {
+                    dist[next.dest] = cur.cost + next.cost;
+                    pq.add(new Node(next.dest,dist[next.dest]));
                 }
             }
         }
-        // dist 순회하면서 k보다 작은거 count 
-        for (int i = 1 ; i<=N; i++){
-            if (dist[i] <= K) answer++;
+        
+        for (int i = 0; i <= N; i++){
+            if (dist[i] <= K){
+                answer++;
+            }
         }
+
         return answer;
     }
 }
