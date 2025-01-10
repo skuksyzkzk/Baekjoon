11cee60d95790b2934f1c9bfe1957e96f29f6ec0
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-01-10 14:29:25 +0900

    [level 1] Title: 모의고사, Time: 4.94 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 77.5 MB, 시간: 3.26 ms
+메모리: 87.5 MB, 시간: 4.94 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 30일 14:46:58
+2025년 01월 10일 14:29:25
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,34 +1,24 @@
 import java.util.*;
 
 class Solution {
+    static int[] first = {1,2,3,4,5};
+    static int[] second =  {2,1,2,3,2,4,2,5};
+    static int[] third = {3,3,1,1,2,2,4,4,5,5};
     public int[] solution(int[] answers) {
-        int[] answer = {};
-        int one = 0;int two = 0; int three = 0;
-        int[] one_arr = {1,2,3,4,5};
-        int[] two_arr = {2,1,2,3,2,4,2,5};
-        int[] three_arr = {3,3,1,1,2,2,4,4,5,5};
-        
-        for (int i = 0; i<answers.length;i++){
-            if (answers[i] == one_arr[i%5]){
-                one++;
-            }
-            if (answers[i] == two_arr[i%8]){
-                two++;
-            }
-            if (answers[i] == three_arr[i%10]){
-                three++;
+        ArrayList<Integer> answer = new ArrayList<>();
+        int[] count = new int[3];
+        for (int i = 0 ; i < answers.length; i++){
+            if (first[i%5] == answers[i]) count[0]++;
+            if (second[i%8] == answers[i]) count[1]++;
+            if (third[i%10] == answers[i]) count[2]++;
+        }
+        int maxValue = Arrays.stream(count).max().getAsInt();
+        for (int i = 0 ; i< 3; i ++){
+            if (count[i] == maxValue){
+                answer.add(i+1);
             }
         }
-        int max_value = Math.max(one,Math.max(two,three));
-        Set<Integer> set = new HashSet<>();
         
-        if (one == max_value)
-            set.add(1);
-        if (two == max_value)
-            set.add(2);
-        if (three == max_value)
-            set.add(3);
-                
-        return set.stream().mapToInt(Integer::intValue).toArray();
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
