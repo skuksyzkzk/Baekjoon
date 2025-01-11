67bbcdf0bcf63f9d46702c3718c74e3952830246
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-01-11 16:13:11 +0900

    [level 2] Title: 방문 길이, Time: 21.01 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,29 +1,49 @@
 import java.util.*;
 
 class Solution {
-    public static boolean isIn(int x,int y) {
-        return x >= 0 && x < 11 && y >= 0 && y <11;
-    }
+    static int x;
+    static int y;
+    
     public int solution(String dirs) {
         int answer = 0;
-        Map<Character,int[]> map = new HashMap<>();
-        
-        map.put('U',new int[]{-1,0});
-        map.put('D',new int[]{1,0});
-        map.put('R',new int[]{0,1});
-        map.put('L',new int[]{0,-1});
-        int x = 5;int y = 5;
-        Set<String> set = new HashSet<>();
-        for (int i = 0; i<dirs.length();i++){
-            int[] move = map.get(dirs.charAt(i));
-            int nx = x + move[0];
-            int ny = y + move[1];
-            if (!isIn(nx,ny)) continue;
-            set.add(x+""+y+","+nx+""+ny);
-            set.add(nx+""+ny+","+x+""+y);
-            x = nx;
-            y = ny;
+        char[] inputs = dirs.toCharArray();
+        HashSet<String> set = new HashSet<>();
+        x = 0 ; 
+        y = 0 ;
+        // 들어온 인풋 만큼 수행 
+        for (int i = 0 ;  i < inputs.length; i++ ){
+            // U 위는 6되면 거부하기
+            int nx = 0; 
+            int ny = 0;
+            if (inputs[i] == 'U' && (y+1) < 6){
+                ny = y+1;
+                set.add(x+"-"+y+" to "+x+"-"+ny);
+                set.add(x+"-"+ny+" to "+x+"-"+y);
+                y++;
+            }
+            // D 아래는 -6되면 거부하기 
+            else if (inputs[i] == 'D' && (y-1) > -6){
+                ny = y-1;
+                set.add(x+"-"+y+" to "+x+"-"+ny);
+                set.add(x+"-"+ny+" to "+x+"-"+y);
+                y--;
+            }
+            // L 왼쪽은 -6되면 거부하기
+            else if (inputs[i] == 'L' && (x-1) > -6){
+                nx = x-1;
+                set.add(x+"-"+y+" to "+nx+"-"+y);
+                set.add(nx+"-"+y+" to "+x+"-"+y);
+                x--;
+            }
+            // R 오른쪽은 6되면 거부하기
+            else if (inputs[i] == 'R' && (x+1) < 6){
+                nx = x+1;
+                set.add(x+"-"+y+" to "+nx+"-"+y);
+                set.add(nx+"-"+y+" to "+x+"-"+y);
+                x++;
+            }
         }
-        return set.size() / 2;
+        
+        return set.size() / 2 ;
     }
 }
