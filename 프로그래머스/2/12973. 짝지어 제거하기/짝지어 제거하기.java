242f17ed2c5be508c74ea230c097b3c7944edcc5
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-01-14 20:18:18 +0900

    [level 2] Title: 짝지어 제거하기, Time: 58.37 ms, Memory: 66.7 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -4,19 +4,16 @@ class Solution
 {
     public int solution(String s)
     {
+        int answer = 0;
+        // 대표적인 스택 문제 
         ArrayDeque<Character> stack = new ArrayDeque<>();
-        for (int i =0;i<s.length();i++){
-            if (stack.isEmpty()){
-                stack.push(s.charAt(i));
-            }
-            else {
-                if (stack.peek() != s.charAt(i)) stack.push(s.charAt(i));
-                else {
-                    stack.pop();
-                }
-            }
+        char[] inputs = s.toCharArray();
+        for (int i = 0; i < inputs.length; i++){
+            if ( stack.isEmpty()) stack.offerLast(inputs[i]);
+            else if ( stack.peekLast() == inputs[i] ) stack.pollLast();
+            else stack.offerLast(inputs[i]);
         }
-        if (stack.isEmpty()) return 1;
-        else return 0;
+        if (stack.isEmpty()) answer = 1;
+        return answer;
     }
 }
