b497d3cb939f9c5318832ca8edc9e23dc2647e13
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-01-14 18:02:38 +0900

    [level 2] Title: 괄호 회전하기, Time: 31.91 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -3,33 +3,28 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        Map<Character, Character> map = new HashMap<>();
+        HashMap<Character,Character> map = new HashMap<>();
         map.put(')','(');
-        map.put(']','[');
         map.put('}','{');
-        
-        int strSize = s.length();
-        for (int startIdx = 0; startIdx < strSize;startIdx++){
+        map.put(']','[');
+    
+        char[] inputs = s.toCharArray();
+        int n = s.length();
+        for (int i = 0; i < n;i++){
             ArrayDeque<Character> stack = new ArrayDeque<>();
-            boolean flag  = true;
-            for (int i = startIdx; i < startIdx + strSize; i++ ){
-                int accessIdx = i % strSize;
-                char target = s.charAt(accessIdx);
-                if (!map.containsKey(target)) stack.push(target);
-                else {
-                    if (stack.isEmpty()){
-                        flag = false;
-                        break;
-                    }
-                    else if (!map.get(target).equals(stack.pop())){
-                        flag = false;
-                        break;
-                    }
+            boolean flag = false;
+            for (int j = i ; j < i + n;j++){
+                if (inputs[j % n ] == '[' || inputs[j % n] == '{' || inputs[j % n] == '('){
+                    stack.offerLast(inputs[j%n]);
                 }
-                
+                else if (stack.isEmpty() || stack.pollLast() != map.get(inputs[j%n]) ){
+                    flag = true;
+                    break;
+                }
             }
-            if (flag && stack.isEmpty()) answer++;
+            if(!flag && stack.isEmpty()) answer++;
         }
+        
         return answer;
     }
 }
