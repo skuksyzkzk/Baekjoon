44ce6185dd970eea48ba3ff60e406ae5302dd3f0
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-01-14 20:53:37 +0900

    [level 2] Title: 주식가격, Time: 25.90 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,27 +1,38 @@
 import java.util.*;
 
 class Solution {
+    class Stock {
+        int order;
+        int price;
+        
+        public Stock(int order,int price){
+            this.order = order;
+            this.price = price;
+        }
+    }
     public int[] solution(int[] prices) {
-        int N = prices.length;
-        // 남는 시간 배열 선언 및 초기화
-        int[] answer = new int[N];
-        ArrayDeque<Integer> stack = new ArrayDeque<>();
-        
-        for (int i = 0; i < N; i++){
-            // 스택이 not empty, 이전 Top이 클 경우
-            while ( !stack.isEmpty() && prices[stack.peek()] > prices[i] ){
-                int targetIdx = stack.pop();
-                answer[targetIdx] = i - targetIdx;   
+        int[] answer = new int[prices.length];
+        int total = prices.length;
+        ArrayDeque<Stock> stack = new ArrayDeque<>();
+        for (int i = 0 ; i < total; i++){
+            Stock cur = new Stock(i+1,prices[i]);
+            
+            if (stack.isEmpty()) stack.offerLast(cur);
+            else if (stack.peekLast().price > cur.price){
+                while (!stack.isEmpty() && stack.peekLast().price > cur.price){
+                    Stock old = stack.pollLast();
+                    answer[old.order-1] = cur.order - old.order;
+                }
+                stack.offerLast(cur);
             }
-            stack.push(i);
+            else {
+                stack.offerLast(cur);
+            }
         }
-        
-        // 최종 스택에서 남은 시간 확정 
-        for (int i : stack){
-            answer[i] = N - 1 - i;
+        while (!stack.isEmpty()){
+            Stock cur = stack.pollLast();
+            answer[cur.order - 1] = total - cur.order;
         }
-        
         return answer;
-        
     }
 }
