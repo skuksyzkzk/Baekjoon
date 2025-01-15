7b2cf8ab6d0776cb999c4474fbc58adb349aac3b
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-01-15 15:04:20 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 124 ms, Memory: 14892 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,35 +1,28 @@
-
-
-
-import java.io.*;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.*;
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+import java.io.*;
 
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-
-        List<Integer> list = new ArrayList<>();
-        for (int i = 1;i <= n; i++){
-            list.add(i);
-        }
-        StringBuilder sb = new StringBuilder();
-        sb.append("<");
-
-        int idx = 0;
-        while(list.size() > 1) {
-            idx = (idx + (k - 1)) % list.size();
-            sb.append(list.remove(idx) + ", ");
-        }
-        sb.append(list.get(0)+">");
-
-        System.out.println(sb.toString());
-
-    }
-
-}
+class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		sb.append("<");
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		List<Integer> list = new ArrayList<>();
+		// 큐에 처음 초기화 1 2 3 4 ~ 
+		for (int i = 1; i <= N; i++) {
+			list.add(i);
+		}
+		int idx = 0;
+		while (list.size() > 1) {
+			idx = (idx + K - 1) % list.size();
+			sb.append(list.remove(idx)+", ");
+		}
+		sb.append(list.get(0)+">");
+		
+		System.out.println(sb.toString());
+		
+	}
+} 
