f67ed696a639c53e8f36b92687b2dbf4a2dffe06
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-01-15 16:59:41 +0900

    [level 2] Title: 기능개발, Time: 2.49 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#) 
 
 ### 성능 요약
 
-메모리: 74.5 MB, 시간: 3.17 ms
+메모리: 77 MB, 시간: 2.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 03일 18:04:56
+2025년 01월 15일 16:59:43
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,18 +1,37 @@
+import java.util.*;
 
-// 문제가 개편되었습니다. 이로 인해 함수 구성이나 테스트케이스가 변경되어, 과거의 코드는 동작하지 않을 수 있습니다.
-// 새로운 함수 구성을 적용하려면 [코드 초기화] 버튼을 누르세요. 단, [코드 초기화] 버튼을 누르면 작성 중인 코드는 사라집니다.
-import java.util.ArrayList;
-import java.util.Arrays;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] dayOfend = new int[100];
-        int day = -1;
-        for(int i=0; i<progresses.length; i++) {
-            while(progresses[i] + (day*speeds[i]) < 100) {
-                day++;
-            }
-            dayOfend[day]++;
+        ArrayList<Integer> answer = new ArrayList<>();
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        for (int i = 0 ; i < progresses.length; i ++) {
+            q.addLast(progresses[i]);
         }
-        return Arrays.stream(dayOfend).filter(i -> i!=0).toArray();
+
+        int days = 0;
+        int idx = 0;
+        int clear = 0;
+        while (!q.isEmpty()){
+            int cur = q.pollFirst();
+            if (cur + days * speeds[idx] >= 100) {
+                clear++;
+                idx++;
+                continue;
+            }
+            if (clear > 0){
+                answer.add(clear);
+                clear = 0;
+            }
+            cur += days * speeds[idx];
+            while ( cur < 100){
+                days++;
+                cur += speeds[idx];
+            }
+            clear++;
+            idx++;
+        }
+        answer.add(clear);
+        
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
