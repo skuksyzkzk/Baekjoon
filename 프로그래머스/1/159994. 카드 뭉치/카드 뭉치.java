5293a3a90cae6c3d31da54e81860212ece11ec77
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-01-15 15:57:39 +0900

    [level 1] Title: 카드 뭉치, Time: 0.63 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -2,36 +2,29 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        StringBuilder sb = new StringBuilder();
-        ArrayDeque<String> c1 = new ArrayDeque<>();
-        ArrayDeque<String> c2 = new ArrayDeque<>();
-        ArrayDeque<String> g = new ArrayDeque<>();
-        
-        for (String s : cards1) {
-            c1.offer(s); // 순서대로 추가
-        }
-        for (String s : cards2) {
-            c2.offer(s); // 순서대로 추가
-        }
-        for (String s : goal) {
-            g.offer(s); // 순서대로 추가
-        }
-        
-        
-        while ( !g.isEmpty() ){
-            String target = g.pollFirst();
-          
-            if ( !c1.isEmpty() && c1.getFirst().equals(target)){
+
+        ArrayDeque<String> c1 = new ArrayDeque<>(Arrays.asList(cards1));
+        ArrayDeque<String> c2 = new ArrayDeque<>(Arrays.asList(cards2));
+        int i = 0;
+        for ( ; i < goal.length; i ++) {
+            String target = goal[i];
+            // c1이 비어있지 않고 c1의 앞의 값이 일치할 경우
+            if (!c1.isEmpty() && target.equals(c1.peekFirst())) {
                 c1.pollFirst();
             }
-            else if ( !c2.isEmpty() &&c2.getFirst().equals(target)){
+            // c2이 비어있지 않고 c2의 앞의 값이 일치할 경우
+            else if (!c2.isEmpty() && target.equals(c2.peekFirst())) {
                 c2.pollFirst();
             }
+            /*
+                1. c1과 c2가 비어있는 경우 
+                2. target 값과 일치하는 게 없는 경우 
+            */
             else {
                 return "No";
             }
-            
         }
+        // 둘다 사용되고 goal이 전부 소진 되어 있는 경우
         
         return "Yes";
     }
