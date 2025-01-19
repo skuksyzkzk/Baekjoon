35e827ab01748f2b67e8095b2287d4a79daa0024
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-01-19 21:46:58 +0900

    [level 2] Title: 할인 행사, Time: 62.00 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,24 +3,25 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        HashMap<String,Integer> map = new HashMap<>();
-        int j  = 0 ;
-        for (String s : want) {
-            map.put(s,number[j]);
-            j++;
+        HashMap<String,Integer> original = new HashMap<>();
+        for (int i= 0 ; i < want.length; i ++){
+            original.put(want[i],number[i]);
         }
-       
-        for (int i = 0 ; i < discount.length - 9; i++){
-            HashMap<String,Integer> cur = new HashMap<>(map);
-           
-            for ( int st = i; st < i + 10; st++){
-                if (cur.getOrDefault(discount[st],0) == 0) continue;
-                cur.put(discount[st], cur.get(discount[st]) - 1);
+        
+        for (int i = 0; i <= discount.length - 10; i++){
+            HashMap<String,Integer> map = new HashMap<>(original);
+            boolean flag = true;
+            for (int j = i; j < i + 10; j++){
+                if (!map.containsKey(discount[j]) || map.get(discount[j]) == 0 ) {
+                    flag = false;
+                    break;
+                }
+                if (map.get(discount[j]) > 0 ){
+                    map.put(discount[j],map.get(discount[j]) - 1);
+                }
             }
+            if (flag) answer++;
            
-            if (cur.values().stream().allMatch(value -> value == 0)){
-                answer++;
-            }
         }
         return answer;
     }
