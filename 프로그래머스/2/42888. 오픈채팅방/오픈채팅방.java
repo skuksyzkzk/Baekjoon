f5a159f16745bb62d9a7d99f59825cd56f4356a7
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-01-19 22:33:23 +0900

    [level 2] Title: 오픈채팅방, Time: 156.87 ms, Memory: 156 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -2,24 +2,24 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] record) {
-        List<String> list = new ArrayList<>();
+        ArrayList<String> answer = new ArrayList<>();
         HashMap<String,String> map = new HashMap<>();
-        for (int i = 0;i < record.length;i++){
-            String[] s = record[i].split(" ");
-            if (s[0].equals("Enter") || s[0].equals("Change")) {
-                map.put(s[1],s[2]);
+        
+        for (int i = 0 ; i< record.length ;i++) {
+            String[] inputs = record[i].split(" ");
+            if (inputs[0].equals("Enter") ||inputs[0].equals("Change")){
+                map.put(inputs[1],inputs[2]);
+            }     
+        }
+        for (int i = 0 ; i< record.length ;i++) {
+            String[] inputs = record[i].split(" ");
+            if (inputs[0].equals("Enter") ){
+                answer.add(map.get(inputs[1])+"님이 들어왔습니다.");
+            } 
+            else if (inputs[0].equals("Leave")){
+                answer.add(map.get(inputs[1])+"님이 나갔습니다.");
             }
         }
-        for (int i = 0; i< record.length;i++){
-            String[] s = record[i].split(" ");
-            if (s[0].equals("Enter")){
-                list.add(map.get(s[1]) + "님이 들어왔습니다.");
-            }
-            else if (s[0].equals("Leave")){
-                list.add(map.get(s[1]) + "님이 나갔습니다.");
-            }
-            
-        }
-        return list.stream().toArray(String[]::new);
+        return answer.stream().toArray(String[]::new);
     }
 }
