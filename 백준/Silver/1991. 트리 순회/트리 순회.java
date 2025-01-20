ac5cf563ec35540bc3ea8771fdadfa965ada38b9
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-01-21 07:42:57 +0900

    [Silver I] Title: 트리 순회, Time: 104 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,74 +1,60 @@
-
 import java.util.*;
 import java.io.*;
 
-public class Main {
-    static class Node {
-        char my;
-        Node left,right;
-
-        public Node(char my,Node left,Node right){
-            this.my = my;
-            this.left = left;
-            this.right = right;
-        }
-    }
-    private static Node head = new Node('A',null,null);
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        StringTokenizer st;
-        for (int i = 0 ; i < n; i ++){
-            st = new StringTokenizer(br.readLine());
-            char root = st.nextToken().charAt(0);
-            char left = st.nextToken().charAt(0);
-            char right = st.nextToken().charAt(0);
-            insertNode(head,root,left,right);
-        }
-        preorder(head);
-        System.out.println();
-        inorder(head);
-        System.out.println();
-        postorder(head);
-    }
-    public static void insertNode(Node head,char root,char left ,char right){
-        if (head.my == root){
-            head.left = (left=='.'?null:new Node(left,null,null));
-            head.right = (right == '.'?null:new Node(right,null,null));
-        }
-        else {
-            if ( head.left != null){
-                insertNode(head.left,root,left,right);
-            }
-            if (head.right != null){
-                insertNode(head.right,root,left,right);
-            }
-        }
-
-    }
-
-    static void preorder(Node x){
-        if (x == null) return;
-        System.out.print(x.my);
-        preorder(x.left);
-        preorder(x.right);
-    }
-    static void inorder(Node x) {
-        if (x == null) return;
-        inorder(x.left);
-        System.out.print(x.my);
-        inorder(x.right);
-    }
-    static void postorder(Node x){
-        if (x == null) return;
-
-        postorder(x.left);
-        postorder(x.right);
-        System.out.print(x.my);
-    }
-
-
-
-
+class Main {
+	static class Node {
+		char value;
+		Node left,right;
+		public Node (char value,Node left,Node right) {
+			this.value = value;
+			this.left = left;
+			this.right = right;
+		}
+	}
+	public static Node head = new Node('A',null,null);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0 ;  i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			char root = st.nextToken().charAt(0);
+			char left = st.nextToken().charAt(0);
+			char right = st.nextToken().charAt(0);
+			insertNode(head,root,left,right);
+		}
+		preOrder(head);
+		System.out.println();
+		inOrder(head);
+		System.out.println();
+		postOrder(head);
+	}
+	public static void insertNode(Node base,char root,char left,char right) {
+		if (base.value == root) {
+			base.left = (left == '.'?null: new Node(left,null,null));
+			base.right = (right == '.'?null: new Node(right,null,null));
+		}
+		else {
+			if (base.left != null) insertNode(base.left, root, left, right);
+			if (base.right != null) insertNode(base.right, root, left, right);
+		}
+	}
+	public static void preOrder(Node find) {
+		if (find == null) return;
+		System.out.print(find.value);
+		preOrder(find.left);
+		preOrder(find.right);
+	}
+	public static void inOrder(Node find) {
+		if (find == null) return;
+		inOrder(find.left);
+		System.out.print(find.value);
+		inOrder(find.right);
+	}
+	public static void postOrder(Node find) {
+		if (find == null) return;
+		postOrder(find.left);
+		postOrder(find.right);
+		System.out.print(find.value);
+	}
 }
