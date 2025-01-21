84fa0baf5915a46221b0412d6be1209fb4fa77ca
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-01-21 09:22:28 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 74.04 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,44 +1,37 @@
 import java.util.*;
+
 class Solution {
+
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        HashMap<String,String> parents = new HashMap<>();
-        parents.put("-","end");
-        // 부모 정보 저장
-        for (int i = 0; i< enroll.length;i++){
-            parents.put(enroll[i],referral[i]);
+        int[] answer = new int[enroll.length];
+        // 이름과 인덱스 정보
+        HashMap<String,Integer> person = new HashMap<>();
+        for (int i = 0 ; i < enroll.length; i++){
+            person.put(enroll[i],i);
         }
-        
-        // 이름과 돈 저장
-        HashMap<String,Integer> moneys = new HashMap<>();
-        for (String s : enroll){
-            moneys.put(s,0);
+        // 자식과 부모관계 
+        HashMap<String,String> relation = new HashMap<>();
+        for (int i = 0 ; i < referral.length;i++){
+            if ("-".equals(referral[i])) relation.put(enroll[i],"center");
+            else relation.put(enroll[i],referral[i]);
         }
-        moneys.put("-",0);
-        for ( int i = 0 ; i < seller.length;i++){
-            String s  = seller[i];
+        // 이익금 계산 
+        for (int i = 0 ; i < seller.length; i++){
             int total = amount[i] * 100;
-    
-            while(!"end".equals(parents.get(s))){
-                int yours = total / 10;
-                int mine = total - yours;
-                if (yours < 1) {
-                    mine = total;
-                }
-                moneys.put(s,moneys.get(s) + mine);
-                if (yours < 1){
-                    break;
-                }
-                s = parents.get(s);
-                total = yours;
+            String target = seller[i];
+            while (!"center".equals(relation.get(target))){
+                if (total == 0 ) break;
+                int index = person.get(target);
+                answer[index] += total - total/10;
+                total /=10;
+                target = relation.get(target);
+            }
+            if (total != 0){
+                int index = person.get(target);
+                answer[index] += total -  total/10;
             }
         }
         
-        int[] answer = new int[enroll.length];
-        int idx=  0;
-        for (String s : enroll){
-            answer[idx] = moneys.get(s);
-            idx++;
-        }
         
         return answer;
     }
