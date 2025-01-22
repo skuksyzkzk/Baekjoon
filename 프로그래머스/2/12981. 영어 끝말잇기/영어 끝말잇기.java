192df72a2f675eed66f279220f0319bfbae0482e
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-01-22 17:18:03 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.16 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,24 +2,30 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        // 가장 먼저 탈락하는 사람의 번호와 그 사람 기준이 몇번째인지 
+        int[] answer = new int[2];
+        int round = 1 ;
         HashSet<String> set = new HashSet<>();
-        int[] count = new int[n];
-        String start = String.valueOf(words[0].charAt(0));
-        for (int i = 0; i<words.length;i++){
-            int who = i % n;
-            count[who]++;
-            if (!words[i].startsWith(start)){
-                return new int[] {who+1,count[who]};
+        set.add(words[0]);
+        int num = 2;
+        for (int i = 1 ; i< words.length;i++){
+            if (set.contains(words[i])) return new int[]{num,round};
+            else {
+                if(words[i].startsWith(String.valueOf(words[i-1].charAt(words[i-1].length() - 1)))){
+                    set.add(words[i]);
+                    num++;
+                }
+                else {
+                    return new int[]{num,round};
+                }
             }
-            if (set.contains(words[i])){
-                return new int[] {who+1,count[who]};
-            }else {
-                set.add(words[i]);
+            if (num > n) {
+                round++;
+                num = 1;
             }
-            start = String.valueOf(words[i].charAt(words[i].length()-1));
+            
         }
-        return new int[] {0,0};
-       
+        
+
+        return answer;
     }
 }
