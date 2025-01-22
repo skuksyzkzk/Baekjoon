93ad1309016a2f3a08a4d5c2e0097cf546f19573
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-01-22 16:52:26 +0900

    [level 1] Title: 폰켓몬, Time: 5.54 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,17 +1,16 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        
-        for (int i = 0; i < nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0 ; i < nums.length; i++){
+            set.add(nums[i]);
         }
-        int N = nums.length / 2;
-        int keyCount = map.keySet().size(); 
-        
-        if (N >= keyCount) return keyCount;
-        else return N;
-        
+        if (set.size() < (nums.length / 2) ) return set.size();
+        else return (nums.length / 2);
+        // N분의 2마리를 선택할때 가장 많은 종류를 선택할 수 있게
+        // 해시로 종류를 기억해서 size 사용 
+        // size보다 n/2가 클 경우 size return
+        // size보다 n/2가 작거나 같을 경우 n/2리턴 
     }
 }
