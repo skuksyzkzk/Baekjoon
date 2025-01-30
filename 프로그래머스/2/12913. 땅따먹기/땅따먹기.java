3d12a2735364c71fcdc5a4f2aff747353c0d54c3
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-01-31 08:57:08 +0900

    [level 2] Title: 땅따먹기, Time: 19.22 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -2,28 +2,18 @@ import java.util.*;
 
 class Solution {
     int solution(int[][] land) {
-       
-        int N = land.length;
-        int [][] dp = new int[N][4];
-        // 자신 위치 제외하고 저장할 공간 3이고 
-        // 첫번째 줄만 갱신 해준다
-        for (int i = 0; i< 4;i++){
+        int[][] dp = new int[land.length][4];
+        for (int i = 0; i < 4; i++){
             dp[0][i] = land[0][i];
         }
-        // 자기 자신 윗 행 제외 3가지 열에 있는 값들 중
-        // 각 열의 3가지 값들중 MAX값을 받아오기
-        for (int i = 1 ; i< N; i++){
-            for (int j= 0; j < 4; j++){
-                int one = (j+1) % 4;
-                int two = (j+2) % 4;
-                int thr = (j+3) % 4;
-                dp[i][j] = land[i][j] + Math.max(dp[i-1][one],Math.max(dp[i-1][thr],dp[i-1][two]));
+        
+        for (int i = 1; i < land.length; i++){
+            for (int j = 0; j < 4; j++){
+                dp[i][j] = land[i][j] + Math.max(dp[i-1][(j+1) % 4], 
+                        Math.max(dp[i-1][(j+2) % 4],dp[i-1][(j+3) % 4]));
             }
         }
-        int max = 0;
-        for (int i = 0; i<4;i++){
-            max = Math.max(max,dp[N-1][i]);
-        }
-        return max;
+
+        return Arrays.stream(dp[land.length-1]).max().getAsInt();
     }
 }
