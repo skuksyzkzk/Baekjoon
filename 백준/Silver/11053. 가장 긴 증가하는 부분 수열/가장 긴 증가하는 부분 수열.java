ab10362cbb2c9f7ece67183ff5ba33d21cf9c6c0
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-01-30 15:37:56 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 124 ms, Memory: 14548 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,34 +1,30 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
+class Main {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] lis = new int [N];
-		int[] arr = new int [N];
-		for (int i = 0; i < N;i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		
+		int[] num = new int[N];
+		int[] dp = new int[N];
+		for (int i = 0 ; i < N ; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
 		}
-		// LIS는 각 해당 위치의 LIS 길이 저장
-		// 조건 : 해당 위치보다 낮지만 해당 숫자보다 작은 LIS중 가장 큰값이랑 + 1 
-		// 만약 같은 값이면 그 값으로
-		lis[0] = 1;
-		for (int i = 1; i < N;i++) {
-			int maxValue = 0 ;
-			for (int j = i-1 ; j >=0;j--) {
-				if (arr[j] > arr[i])continue;
-				else if (arr[j] < arr[i] && maxValue < lis[j]) {
-					maxValue = lis[j];
+		for (int k = 0 ; k < N;  k++) {
+			dp[k] = 1 ;
+			for (int i = 0 ; i < k; i++) {
+				if (num[i] < num[k]) {
+					dp[k] = Math.max(dp[k],dp[i]+1);
 				}
 			}
-			lis[i] = maxValue + 1;
 		}
-		int maxNum = 0 ;
-		maxNum = Arrays.stream(lis).max().getAsInt();
-		System.out.println(maxNum);
-		
+        int maxValue = 0 ; 
+		for (int i = 0; i < N ; i++){
+            if (maxValue < dp[i]) maxValue = dp[i];
+        }
+        System.out.println(maxValue);
 	}
-
 }
