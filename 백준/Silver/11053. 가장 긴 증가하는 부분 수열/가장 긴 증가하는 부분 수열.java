1558b70d7066153b22fe9f0f07dc45086bf2c8a5
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-01-30 16:29:21 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 112 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -2,29 +2,50 @@ import java.util.*;
 import java.io.*;
 
 class Main {
+	static int[] num;
+	static int[] dp;
+	static int[] rec;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		int N = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
 		
-		int[] num = new int[N];
-		int[] dp = new int[N];
-		for (int i = 0 ; i < N ; i++) {
+		num = new int[N];
+		dp = new int[N];
+		rec = new int[N];
+		
+ 		for (int i = 0 ; i < N ; i++) {
 			num[i] = Integer.parseInt(st.nextToken());
 		}
-		for (int k = 0 ; k < N;  k++) {
-			dp[k] = 1 ;
-			for (int i = 0 ; i < k; i++) {
-				if (num[i] < num[k]) {
-					dp[k] = Math.max(dp[k],dp[i]+1);
-				}
-			}
+ 		int j = 0;
+ 		dp[j] = num[0];
+ 		int i = 1;
+ 		while (i < N) {
+ 			// 현재 LIS의 마지막 보다 크면 순열에 추가 
+ 			if (dp[j] < num[i]) {
+ 				j+=1;
+ 				dp[j] = num[i];
+ 			}
+ 			// 작을 경우 올바른 위치 찾아서 삽입하기 
+ 			else {
+ 				int pos = binarySearch(0,j,num[i]);
+ 				dp[pos] = num[i];
+ 			}
+ 			i++;
+ 		}
+		System.out.println(j+1);
+	}
+
+	private static int binarySearch(int i, int j, int find) {
+		int mid = 0;
+		int lt = i, rt = j;
+		while (lt < rt) {
+			mid = (lt+rt)/2 ;
+			if (dp[mid] < find) lt = mid+1;
+			else rt = mid;
 		}
-        int maxValue = 0 ; 
-		for (int i = 0; i < N ; i++){
-            if (maxValue < dp[i]) maxValue = dp[i];
-        }
-        System.out.println(maxValue);
+		return rt ;
 	}
 }
