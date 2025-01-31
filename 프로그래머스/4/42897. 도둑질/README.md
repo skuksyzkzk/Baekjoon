9d4d12bcc6b210d148d5f73de0a6ff5a34b827b4
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-01-31 19:28:21 +0900

    [level 4] Title: 도둑질, Time: 37.23 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/README.md
+++ 프로그래머스/4/42897. 도둑질/README.md
@@ -1,10 +1,10 @@
 # [level 4] 도둑질 - 42897 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42897#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42897) 
 
 ### 성능 요약
 
-메모리: 102 MB, 시간: 36.76 ms
+메모리: 102 MB, 시간: 37.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 19일 19:54:08
+2025년 01월 31일 19:28:18
 
 ### 문제 설명
 
--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -2,33 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] money) {
-        int N = money.length;
-        int[] dp1 = new int[N];
-        int[] dp2 = new int[N];
-        
-        // 첫번째 집 선탟 3번째까지 정해짐 
-        dp1[0] = money[0]; dp1[1] = money[0]; dp1[2] = money[0] + money[2];
-        // 첫번째 집 선택 안하는 경우는 2번째까지만 정해짐 
-        dp2[0] = 0 ; dp2[1] = money[1];
-        // 첫번째 집을 선택한 경우, 마지막 집 선택불가 
-        // i-2번째 까지 합과 i-3번째 까지 합중 비교해서 MAX값을 현재값에 더해야된다.
-        for (int i = 3 ;i <N-1;i++){
-            dp1[i] = money[i] + Math.max(dp1[i-2],dp1[i-3]);
+        int[] dp1 = new int[money.length+1];
+        int[] dp2 = new int[money.length+1];
+        // 첫번째 집을 들릴 경우 -> 마지막 집 무조건 안들린다.
+        dp1[1] = money[0]; dp1[2] = money[0]; dp1[3] = dp1[1] + money[2];
+        for (int i = 4; i < money.length; i++){
+            dp1[i] = Math.max(dp1[i-1], dp1[i-2] + money[i-1]);
         }
-        // 첫번째 집을 선택하지 않는 경우는 선택을 해야한다.
-        // i-1 값과 i-2 + 현재값을 비교해서 큰값을 선택한다.
-        for (int i = 2; i< N;i++){
-            dp2[i] = Math.max(dp2[i-1],money[i] + dp2[i-2]);
-        }
-        // 최종적으로 dp1[N-2] 값과 dp2[N-1]값 비교
-        int answer = 0;
-        for (int i = 0; i<N;i++){
-            answer= Math.max(answer,dp1[i]);
-        }
-        for (int i = 0; i<N;i++){
-            answer= Math.max(answer,dp2[i]);
+        // 첫번째 집을 들리지 않을 경우
+        dp2[1] = 0; dp2[2] = money[1]; dp2[3] = Math.max(money[1],money[2]);
+        for (int i = 4; i <= money.length; i++){
+            dp2[i] = Math.max(dp2[i-1],dp2[i-2] + money[i-1]);
         }
         
-        return answer;
+       
+        
+        int maxValue = Arrays.stream(dp1).max().getAsInt();
+        if (maxValue < Arrays.stream(dp2).max().getAsInt()) 
+            maxValue = Arrays.stream(dp2).max().getAsInt();
+        return maxValue;
     }
 }
