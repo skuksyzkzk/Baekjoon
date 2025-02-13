5ef7ed99121000c4435fe86b67d1e853f0bba13d
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-02-13 21:04:57 +0900

    [Gold IV] Title: 타임머신, Time: 216 ms, Memory: 17040 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -1,77 +1,74 @@
-
 import java.util.*;
 import java.io.*;
 
-class Node {
-    int dest, cost;
-    public Node(int dest, int cost) {
-        this.dest = dest;
-        this.cost = cost;
-    }
-}
+class Main{
+	static class Edge {
+		int src,dest,cost;
+		public Edge (int src,int dest,int cost) {
+			this.src = src;
+			this.dest = dest;
+			this.cost = cost;
+		}
+	}
+	static int N,M;
+	static ArrayList<Edge> adj;
+	static long[] dist;
+	
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		adj = new ArrayList<>();
+		dist = new long[N+1];
+		Arrays.fill(dist, Long.MAX_VALUE);
+		
+		for (int i = 0 ; i < M ; i++) {
+			st = new StringTokenizer(br.readLine());
+			int src = Integer.parseInt(st.nextToken());
+			int dest = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
+			
+			adj.add(new Edge(src,dest,cost));
+		}
+		StringBuilder sb = new StringBuilder();
+		// 무한이 시간을 돌리는 경우
+		if (!BFM(1)) {
+			sb.append(-1);
+			System.out.println(sb);
+			return;
+		}
+		// 정상적인 상황
+		for (int i = 2; i <= N; i++) {
+			if (dist[i] == Long.MAX_VALUE) sb.append(-1+"\n");
+			else sb.append(dist[i] + "\n");
+		}
+		
+		System.out.println(sb.toString());
+	}
 
-public class Main {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        // 초기화
-        ArrayList<Node>[] adjList = new ArrayList[n + 1];
-        for (int i = 0; i <= n; i++) {
-            adjList[i] = new ArrayList<>();
-        }
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int source = Integer.parseInt(st.nextToken());
-            int dest = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-
-            adjList[source].add(new Node(dest, cost));
-        }
-
-        long[] dist = new long[n + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[1] = 0;
-
-        // v-1 번 반복해서 간선 업데이트
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 1; j <= n; j++) {
-                for (Node node : adjList[j]) {
-                    if (dist[j] != Integer.MAX_VALUE && dist[node.dest] > dist[j] + node.cost) {
-                        dist[node.dest] = dist[j] + node.cost;
-                    }
-                }
-            }
-        }
-
-        // 한번 더 반복해서 값이 변했으면 -1 즉 음의 사이클 존재
-        boolean hasNegativeCycle = false;
-        outer:
-        for (int j = 1; j <= n; j++) {
-            for (Node node : adjList[j]) {
-                if (dist[j] != Integer.MAX_VALUE && dist[node.dest] > dist[j] + node.cost) {
-                    System.out.println("-1");
-                    hasNegativeCycle = true;
-                    break outer;  // 음의 사이클 발견 시 바로 종료
-                }
-            }
-        }
-
-        // 음의 사이클이 없으면 결과 출력
-        if (!hasNegativeCycle) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 2; i <= n; i++) {
-                if (dist[i] == Integer.MAX_VALUE) {
-                    sb.append("-1\n");
-                } else {
-                    sb.append(dist[i]).append("\n");
-                }
-            }
-            System.out.print(sb);
-        }
-    }
+	private static boolean BFM(int st) {
+		// TODO Auto-generated method stub
+		dist[st] = 0;
+		
+		for (int i = 0; i < N-1; i++) {
+			
+			for (Edge edge : adj) {
+				if (dist[edge.src] == Long.MAX_VALUE) continue;
+				if (dist[edge.dest] > dist[edge.src] + edge.cost) {
+					dist[edge.dest] = dist[edge.src] + edge.cost;
+				}
+			}
+		}
+		// 음의 사이클
+		for (Edge edge : adj) {
+			if (dist[edge.src] == Long.MAX_VALUE) continue;
+			if (dist[edge.dest] > dist[edge.src] + edge.cost) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
 }
