8d9ddc42902880278d37dc08b06d006bbc3f88b7
skuksyzkzk <129512374+skuksyzkzk@users.noreply.github.com>
2025-02-27 14:27:29 +0900

    [level 2] Title: 튜플, Time: 73.98 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,23 +2,23 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
-        String temp = s.substring(0,s.length()-2).replace("{","");
-        String[] inputs = temp.split("},");
+        String input = s.substring(0,s.length()-2).replace("{","");
+        String[] save = input.split("},");
         
-        Arrays.sort(inputs, (a,b) -> Integer.compare(a.length(),b.length()));
+        Arrays.sort(save,(a,b) -> Integer.compare(a.length(),b.length()));
+        int[] answer = new int[save.length];
         HashSet<String> set = new HashSet<>();
-        int[] answer = new int[inputs.length];
-        
-        int i = 0;
-        for (String str : inputs){
-            String[] numbers = str.split(",");
-            for (String num : numbers){
-                if(!set.contains(num)){
-                    answer[i++] = Integer.parseInt(num);
-                    set.add(num);
+        int idx = 0;
+        for (String str : save){
+            String[] temp = str.split(",");
+            for (String a : temp) {
+                if(!set.contains(a)){
+                    answer[idx++] = Integer.parseInt(a);
+                    set.add(a);
                 }
             }
         }
+        
         return answer;
     }
 }
